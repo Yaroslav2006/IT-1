7a9a285925990c85fb785d9f93b966d2a2ab6ffe
Yaroslav2006 <avtolife2006@gmail.com>
2018-02-12 19:48:35 +0200

    Add files via upload

---
+++ config.php
@@ -0,0 +1,5 @@
+<?php
+define('DB_HOST', 'localhost');
+define('DB_DB', 'oop-step-game');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
--- post.php
+++ post.php
@@ -1,11 +1,18 @@
 <?php
-$url = '';
-foreach ($_POST as $key => $value) {
-    $url .= $key . '=' . $value;
-    if (end ($_POST) != $value){
-    $url .= '$';
-    }
-}
-echo $url . '<br>
-    $redirect = 'localhost:63342/IT-SCHOOL/PHP/PHP/get.php/' . $url;
-    header('Location: ' . $redirect);
+ini_set("display_errors", 1);
+
+    $name = $_POST['name'];
+    $image = $_POST['image'];
+    $hp = $_POST['hp'];
+    $mp = $_POST['mp'];
+    $ap = $_POST['ap'];
+    $armor_type = $_POST['armor_type'];
+    $speed = $_POST['speed'];
+    $damage = $_POST['damage'];
+    $damage_type = $_POST['damage_type'];
+    $skills_id = $_POST['skills_id'];
+    $gain_xp = $_POST['gain_xp'];
+    $gain_price = $_POST['gain_price'];
+
+require_once('config.php');
+$db = new MySQLi(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_DB);
